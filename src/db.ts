6121505f23a56ebcd8ed29cc7de92574d22df88a
Vaibhav Shukla <vaibhav.shukla0925@gmail.com>
2025-08-13 13:01:55 +0530

    Created signup, signup end points and configuration of data base as well as defini of user and content schema

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,27 @@
-import { Model, Schema } from "mongoose";
+import mongoose, { model, Schema } from "mongoose";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const mongooURL = process.env.Database_URL!;
+
+
+await mongoose.connect(mongooURL);
 
 const UserSchema = new Schema({
     username: {type: String, unique: true},
     password: String
 })
 
-export const UserModel = new Model(UserSchema, "users");
+export const UserModel = model("User", UserSchema);
+
+
+const ContentSchema = new Schema({
+    tittle: String,
+    link: String,
+    tags: [{type: mongoose.Types.ObjectId, ref: "Tags"}],
+    userId: {type: mongoose.Types.ObjectId, ref: "User", require: true}
+})
+
+
+export const ContentModel = model("Content", ContentSchema)
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,60 @@
 import express from "express";
 import mongoose from "mongoose"
 import jwt from 'jsonwebtoken'
+import { UserModel } from "./db.js";
+import { JWT_Secret } from "./config.js";
+
 
 const app = express();
+app.use(express.json());
 
-app.post("/api/v1/signup", (req, res) => {
+app.post("/api/v1/signup", async(req, res) => {
+    // Include zod validation and hash the password
+    const username = req.body.username;
+    const password = req.body.password;
 
+    try{
+        await UserModel.create({
+        username: username,
+        password: password
+        })
+
+        res.json({
+            message: "You are signUp"
+        })
+    }catch(e){
+        res.status(411).json({
+            message: "User Alreay Exist"
+        })
+    }
+    
 })
 
-app.post("/api/v1/signin", (req, res) => {
+app.post("/api/v1/signin", async(req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    })
+    if (existingUser){
+        const token = jwt.sign({
+            id: existingUser._id
+        }, JWT_Secret)
 
+        res.json({
+            token
+        })
+    }else{
+        res.status(403).json({
+            message: "Incorrect Credentials"
+        })
+    }
 })
 
 app.post("/api/v1/content", (req, res) => {
-
+    const link = req.body.link;
+    const type = req.body.type
 })
 
 app.get("/api/v1/content", (req, res) => {
@@ -31,3 +72,5 @@ app.post("/api/v1/brain/share", (req, res) => {
 app.get("/api/v1/brain/:shareLink", (req, res) => {
     
 })
+
+app.listen(3000);
